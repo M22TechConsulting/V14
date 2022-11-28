be83c03af038668b3812b9e96214cd84ebb11f34
Tech Consulting <94559660+M22TechConsulting@users.noreply.github.com>
2022-11-28 10:27:45 -0700

    Add files via upload
    google_auth_oauthlib

---
+++ google-auth-library-python-oauthlib-main/owlbot.py
@@ -0,0 +1,40 @@
+import synthtool as s
+from synthtool import gcp
+
+common = gcp.CommonTemplates()
+
+# ----------------------------------------------------------------------------
+# Add templated files
+# ----------------------------------------------------------------------------
+templated_files = common.py_library(
+    microgenerator=True,
+    cov_level=99,
+    unit_test_external_dependencies=["click"],
+    unit_test_python_versions=["3.6", "3.7", "3.8", "3.9", "3.10", "3.11"],
+)
+s.move(templated_files, excludes=[
+    "docs/multiprocessing.rst",
+    "README.rst"
+])
+
+# Change black paths
+s.replace(
+    "noxfile.py",
+    """LINT_PATHS =.*""",
+    """LINT_PATHS = ["docs", "google_auth_oauthlib", "tests", "noxfile.py", "setup.py"]""",
+)
+
+# Change flake8 paths
+s.replace(
+    "noxfile.py",
+    'session.run\("flake8", "google", "tests"\)',
+    'session.run("flake8", *LINT_PATHS)',
+)
+
+s.replace(
+    "noxfile.py",
+    '"--cov=google",',
+    '"--cov=google_auth_oauthlib",',
+)
+
+s.shell.run(["nox", "-s", "blacken"], hide_output=False)
